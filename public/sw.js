234b39a0b56f216588eac125f2a5796cb0bfed1a
Dan Bovey <dan@danbovey.uk>
2017-02-17 02:30:43 +0000

    Fetch with service worker

--- public/sw.js
+++ public/sw.js
@@ -21,3 +21,16 @@ this.addEventListener('install', event => {
             ]))
     );
 });
+
+this.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if(response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
